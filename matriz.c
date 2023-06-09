daae1be3805801db8df5d03af3897790c845e692
Eduardo Caetano <EduardoCaetano110@gmail.com>
2023-06-09 08:20:49 -0300

    Matriz 3 x 3

--- matriz.c
+++ matriz.c
@@ -1,18 +1,20 @@
 #include <stdio.h>
 
 void main(){
-
+//Primeiro [3] = Linha.
+//Segundo [3] = Coluna.
 int matriz3x3 [3] [3]= {{1, 6, 9},
                         {5 , 2, 4},
                         {7 ,0 ,1}};
 
+
 for(int i = 0; i < 3;i++){
     for(int j = 0; j < 3; j++){
-        printf(" %i ", matriz3x3[i] [j]);
-        
+        //Multiplicação dela por 5.
+        matriz3x3[i][j] *= 5;
+        printf(" %i| ", matriz3x3[i] [j]);
+        }
+    printf(" \n");
     }
-    printf("\n");
 
-    
-}
 }
