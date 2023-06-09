4e214865c188df9f7c50963c683ae226906bf859
Eduardo Caetano <EduardoCaetano110@gmail.com>
2023-06-09 07:26:00 -0300

    Matriz bidimencional

--- matriz.c
+++ matriz.c
@@ -6,5 +6,13 @@ int matriz3x3 [3] [3]= {{1, 6, 9},
                         {5 , 2, 4},
                         {7 ,0 ,1}};
 
+for(int i = 0; i < 3;i++){
+    for(int j = 0; j < 3; j++){
+        printf(" %i ", matriz3x3[i] [j]);
+        
+    }
+    printf("\n");
 
+    
+}
 }
