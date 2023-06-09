bfaa44dac8356a3795dee59042c12976ca1d468c
Eduardo Caetano <EduardoCaetano110@gmail.com>
2023-06-09 06:26:59 -0300

    Verificação de par e ímpar.

---
+++ repeticaofor.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+
+void main(){
+// Configuração da página de código UTF-8
+    unsigned int CPAGE_UTF8 = 65001;
+    unsigned int CPAGE_DEFAULT = GetConsoleOutputCP();
+    SetConsoleOutputCP(CPAGE_UTF8);
+
+    int numIni = 0;
+    int numFinal = 0;
+    int quantidadeTotalDePar = 0;
+
+    printf("Digite um número inicial: ");
+    scanf("%d", &numIni);
+
+    printf("Digite um número Final: ");
+    scanf("%d", &numFinal);
+
+    if (numFinal < numIni ){
+        numIni= 0;
+        numFinal= 0;
+        printf("Não foi possivel realizar essa tarefa.");
+    }
+        
+        for (int i = numIni; i <= numFinal; i++ ){
+        
+            if( i % 2 == 0){
+                printf("o valor do número -> %d é par! \n", i);
+                quantidadeTotalDePar++;
+
+            }else {
+                printf("o valor do número -> %d é impar! \n", i);
+            }
+        }
+        printf("O total de pares é: %d\n", quantidadeTotalDePar); 
+}
