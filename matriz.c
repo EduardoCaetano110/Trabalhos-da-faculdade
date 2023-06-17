b6d7c84824977a5606ecd970adbf5b2c15520663
Eduardo Caetano <EduardoCaetano110@gmail.com>
2023-06-17 11:37:56 -0300

    Matrix3x3

--- matriz.c
+++ matriz.c
@@ -9,12 +9,13 @@ int matriz3x3 [3] [3]= {{1, 6, 9},
 
 
 for(int i = 0; i < 3;i++){
+    printf("|");
     for(int j = 0; j < 3; j++){
         //Multiplicação dela por 5.
         matriz3x3[i][j] *= 5;
-        printf(" %i| ", matriz3x3[i] [j]);
+        printf(" %i", matriz3x3[i] [j]);
         }
-    printf(" \n");
-    }
+    printf("|\n");
 
+}
 }
